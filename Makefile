2c2e74dab54d1cc6547f3240a7eb93f2e1b07236
Andy Belle-Isle <drumsetmonkey@gmail.com>
2021-05-20 03:53:51 -0400

    Added ChibiOS example for STM32L476G-Disco
    * Will probably change up makefile layout

--- Makefile
+++ Makefile
@@ -4,11 +4,11 @@ CHIBIOS_CONTRIB_VERSION=chibios-20.3.x
 CHIBIOS=os/ChibiOS
 CHIBIOS_CONTRIB=os/ChibiOS-Contrib
 
-DEVICE=NRF52832
+DEVICE=STM32L476-DISCOVERY
+
+ALLCPPSRC += ./src/main.cpp
 
 include arch/$(DEVICE)/arch.mk
-
-all:
 
 update:
 	git subtree pull --prefix os/ChibiOS https://github.com/ChibiOS/ChibiOS $(CHIBIOS_VERSION) --squash
---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include "ch.h"
+#include "hal.h"
+
+// Thread for LED status and wakeup hold
+__attribute__((section(".stacks")))
+static THD_WORKING_AREA(monitorThreadWA, 256);
+static THD_FUNCTION(monitorThread, arg);
+
+int main(void)
+{
+    halInit();
+    chSysInit();
+
+    chThdCreateStatic(
+        monitorThreadWA, sizeof(monitorThreadWA),
+        LOWPRIO,
+        monitorThread, nullptr);
+
+    chThdExit(0);
+    return 0;
+}
+
+THD_FUNCTION(monitorThread, arg)
+{
+    (void)arg;
+    while(true) {
+        palClearLine(LINE_LED_GREEN);
+        chThdSleepMilliseconds(500);
+        palClearLine(LINE_LED_RED);
+        chThdSleepMilliseconds(500);
+        palSetLine(LINE_LED_GREEN);
+        chThdSleepMilliseconds(500);
+        palSetLine(LINE_LED_RED);
+        chThdSleepMilliseconds(500);
+    }
+}
