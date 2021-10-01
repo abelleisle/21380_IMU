9980f46a9205cbce31002346c6e0ef8d166e7542
Andy Belle-Isle <drumsetmonkey@gmail.com>
2021-10-01 03:30:51 -0400

    Added packed IMU data for advertising
    * Added packed data for sensor_value
      * Whole value is just an int8_t
      * Fractional value is int16_t shifted right 8 bits (millionth fraction)
    
    * Required callback for IMU now

--- firmware/src/imu/IMU.cpp
+++ firmware/src/imu/IMU.cpp
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <zephyr.h>
 #include <cstring>
+#include <stdint.h>
 
 #include <memory>
 
@@ -18,10 +19,25 @@ namespace IMU
     sensor_value temperature;
     sensor_value accel[3];
     sensor_value gyro[3];
+    imu_data data;
 
     void handle_imu_data(const struct device *dev, struct sensor_trigger *trig);
 
-    int init(const char *const label)
+    imu_packed& imu_packed::operator= (sensor_value &v)
+    {
+        whole = static_cast<int8_t>(v.val1);
+        fractional = static_cast<int16_t>(v.val2 >> 8);
+
+        return *this;
+    }
+
+    void imu_packed::print(void)
+    {
+        float res = whole + (float)(fractional << 8)/1000000.0f;
+        printf("%g\n", res);
+    }
+
+    int init(const char *const label, std::function<void()> callback)
     {
         hw.reset(device_get_binding(label));
 
@@ -40,12 +56,14 @@ namespace IMU
             return 1;
         }
 
+        cb = callback;
+
         return 0;
     }
 
-    int setCallback(std::function<void()> _cb)
+    imu_data* raw()
     {
-        cb = _cb;
+        return &data;
     }
 
     int process_imu_data(const struct device *dev)
@@ -69,13 +87,20 @@ namespace IMU
             memcpy(accel, tmp_accel, 3*sizeof(sensor_value));
             memcpy(gyro, tmp_gyro, 3*sizeof(sensor_value));
 
-            printf("[%s]:%g Cel\n"
-                "  accel %f %f %f m/s/s\n"
-                "  gyro  %f %f %f rad/s\n",
-                "12:00", sensor_value_to_double(&temperature),
-                sensor_value_to_double(&accel[0]), sensor_value_to_double(&accel[1]),
-                sensor_value_to_double(&accel[2]), sensor_value_to_double(&gyro[0]),
-                sensor_value_to_double(&gyro[1]), sensor_value_to_double(&gyro[2]));
+            data.timestamp = k_uptime_get_32();
+            data.temperature = temperature;
+            for (int i = 0; i < 3; i++) {
+                data.accel[i] = accel[i];
+                data.gyro[i] = gyro[i];
+            }
+
+            /* printf("[%s]:%g Cel\n" */
+            /*     "  accel %f %f %f m/s/s\n" */
+            /*     "  gyro  %f %f %f rad/s\n", */
+            /*     "12:00", sensor_value_to_double(&temperature), */
+            /*     sensor_value_to_double(&accel[0]), sensor_value_to_double(&accel[1]), */
+            /*     sensor_value_to_double(&accel[2]), sensor_value_to_double(&gyro[0]), */
+            /*     sensor_value_to_double(&gyro[1]), sensor_value_to_double(&gyro[2])); */
 
             // TODO: error handle this
             cb();
@@ -97,3 +122,59 @@ namespace IMU
         }
     }
 }
+
+  /*
+  const char *const label = DT_LABEL(DT_INST(0, invensense_icm42688));
+  const struct device *icm42688 = device_get_binding(label);
+  const struct device *led = device_get_binding(LED0);
+
+  if (led == NULL) {
+    printk("Failed to initialize diagnostic LED");
+    return;
+  }
+
+  int ret = gpio_pin_configure(led, PIN, GPIO_OUTPUT_ACTIVE | FLAGS);
+  if (ret < 0) {
+    printk("Could not configure LED");
+    return;
+  }
+
+  if (!icm42688) {
+    printf("Failed to find sensor %s\n", "ICM42688");
+    return;
+  }
+
+  tap_trigger = (struct sensor_trigger){
+      .type = SENSOR_TRIG_TAP,
+      .chan = SENSOR_CHAN_ALL,
+  };
+
+  if (sensor_trigger_set(icm42688, &tap_trigger, handle_icm42688_tap) < 0) {
+    printf("Cannot configure tap trigger!!!\n");
+    return;
+  }
+
+  double_tap_trigger = (struct sensor_trigger){
+      .type = SENSOR_TRIG_DOUBLE_TAP,
+      .chan = SENSOR_CHAN_ALL,
+  };
+
+  if (sensor_trigger_set(icm42688, &double_tap_trigger,
+                         handle_icm42688_double_tap) < 0) {
+    printf("Cannot configure double tap trigger!!!\n");
+    return;
+  }
+
+  data_trigger = (struct sensor_trigger){
+      .type = SENSOR_TRIG_DATA_READY,
+      .chan = SENSOR_CHAN_ALL,
+  };
+
+  if (sensor_trigger_set(icm42688, &data_trigger, handle_icm42688_drdy) < 0) {
+    printf("Cannot configure data trigger!!!\n");
+    return;
+  }
+
+  printf("Configured for triggered sampling.\n");
+
+  */
--- firmware/src/imu/IMU.hpp
+++ firmware/src/imu/IMU.hpp
@@ -2,9 +2,30 @@
 
 #include <stdint.h>
 #include <functional>
+#include <drivers/sensor.h>
 
 namespace IMU
 {
-    int init(const char* const label);
-    int setCallback(std::function<void()> _cb);
+    struct imu_packed {
+        int8_t whole;
+        int16_t fractional;
+
+        void print(void);
+        imu_packed& operator= (sensor_value &v);
+    } __attribute__((packed));
+
+    struct imu_data {
+        union {
+            struct {
+                int16_t timestamp;
+                imu_packed temperature;
+                imu_packed accel[3];
+                imu_packed gyro[3];
+            };
+            uint8_t raw[2 + sizeof(imu_packed)*7];
+        };
+    } __attribute__((packed));
+
+    int init(const char* const label, std::function<void()> callback);
+    imu_data* raw();
 }
